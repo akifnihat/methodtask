0a832b31860633927a05cb54076b47ebd3e0f517
akifnihat <akifst@code.edu.az>
2024-02-21 17:38:57 +0400

    methodtask

---
+++ methodtask/Program.cs
@@ -0,0 +1,74 @@
+﻿//namespace methodtask
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello, World!");
+
+
+//            Console.WriteLine(divide(5, 2));
+//            Console.WriteLine(arr(11, 4, 5, 7, 21));
+//            Console.WriteLine(tek(5, 12));
+//            Console.WriteLine(sademurekkeb(7));
+//            Console.WriteLine(quvvet(9));
+
+//        }
+//        #region task1
+//        static int divide(int a, int b)
+//        {
+//            return a / b;
+//        }
+//        #endregion
+//        #region tsk2
+//        static int arr(params int[] numbers)
+//        {
+//            int sum = 0;
+//            for (int i = 0; i < numbers.Length; i++)
+//            {
+//                sum += numbers[i];
+//            }
+//            return sum;
+//        }
+//        #endregion
+//        #region task3
+//        static int tek(int n, int m)
+//        {
+//            int count = 0;
+//            for (int i = n; i < m; i++)
+//            {
+//                if (i % 2 == 1)
+//                {
+//                    count++;
+//                }
+//            }
+//            return count;
+//        }
+//        #endregion
+//        #region task4 yarimciq qaldi
+//        static string sademurekkeb(double x)
+//        {
+//            int count = 0;
+//            for (int i = n; i < m; i++)
+         
+//        }
+//        #endregion
+//        #region task5
+//        static string quvvet(double n)
+//        {
+//            while (n>1)
+//            {
+//                n /= 2;
+//            }
+//            if (n == 1)
+//            {
+//                return "quvvetidir";
+//            }
+//            else
+//            {
+//                return "quvveti deyil";
+//            }
+//        }
+//        #endregion
+//    }
+//}
